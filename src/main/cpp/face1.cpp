a4c0c2de2b661c417f97f81ffe5061685632c6bd
masgari <mammad.asgari@gmail.com>
2013-10-06 20:52:36 +0100

    Update face1.cpp

--- src/main/cpp/face1.cpp
+++ src/main/cpp/face1.cpp
@@ -23,26 +23,6 @@ int main(int argc, char* argv[]) {
     vector<Rect> faces;
     classifier.detectMultiScale(img, faces);
     printf("Faces detected:%d\n", faces.size());
-    /*
-    CvMemStorage* storage = cvCreateMemStorage(0);
-    CvHaarClassifierCascade* cascade = (CvHaarClassifierCascade*)cvLoad( "haarcascade_frontalface_alt2.xml" );
-    double scale = 1.3;
-
-    static CvScalar colors[] = { {{0,0,255}}, {{0,128,255}}, {{0,255,255}}, 
-    {{0,255,0}}, {{255,128,0}}, {{255,255,0}}, {{255,0,0}}, {{255,0,255}} };
-
-    // Detect objects
-    cvClearMemStorage( storage );
-    CvSeq* objects = cvHaarDetectObjects( img, cascade, storage, 1.1, 4, 0, cvSize( 40, 50 ));
-
-    CvRect* r;
-    // Loop through objects and draw boxes
-    for( int i = 0; i < (objects ? objects->total : 0 ); i++ ){
-            r = ( CvRect* )cvGetSeqElem( objects, i );
-            cvRectangle( img, cvPoint( r->x, r->y ), cvPoint( r->x + r->width, r->y + r->height ),
-                    colors[i%8]);
-    }
-     */
 
     Scalar border(0,255,0);
     for (size_t i = 0; i < faces.size(); i++){
