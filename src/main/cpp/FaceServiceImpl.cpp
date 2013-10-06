001b65d97bfa75f2c7cc979ea8f60ad3fc3c77db
mamad <mammad.asgari@gmail.com>
2013-10-06 21:06:24 +0100

    initial commit

--- src/main/cpp/FaceServiceImpl.cpp
+++ src/main/cpp/FaceServiceImpl.cpp
@@ -5,7 +5,21 @@
  * Created on 06 October 2013, 20:34
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/contrib/contrib.hpp>
+
+using namespace std;
+using namespace cv;
+
+
 #include "FaceServiceImpl.h"
+
+Mat* readImageFromRequest(TFaceDetectResponse& _return, const TImage& request);
 
 FaceServiceImpl::FaceServiceImpl(const int8_t threads) {
 }
@@ -19,5 +33,20 @@ int8_t FaceServiceImpl::handlePing() {
 }
 
 void FaceServiceImpl::handleDetectFaces(TFaceDetectResponse& _return, const TImage& request) {
-    
+    //Mat* img = new Mat();
+    //imdecode();
+    Mat* img = readImageFromRequest(_return, request);
+    if (img == NULL) {
+        return;
+    }
+}
+
+Mat* readImageFromRequest(TFaceDetectResponse& _return, const TImage& request) {
+    if (request.data.size() < 1) {
+        _return.__set_error("Invalid data");
+        return NULL;
+    }
+    cout << "request image w:" << request.width << ", h:" << request.height << "\n";
+
+    return NULL;
 }
---
+++ src/pfs-client/lib/pfs-client.js
@@ -0,0 +1,112 @@
+var thrift = require('thrift'),
+    ImageService = require('./FaceService.js'),
+    ttypes = require('./service_types.js'),
+    util = require('util'),
+    events = require('events');
+
+/**
+ * default configurations for Peach Image Server Client
+ * @type {{host: string, port: number}}
+ */
+var DEFAULTS = {
+    host: '127.0.0.1',
+    port: 3800
+};
+
+/**
+ * Create a new client
+ * @param options
+ * @constructor
+ */
+function Client(options) {
+    options = options || {};
+    this.options = merge(options, DEFAULTS);
+}
+
+/**
+ *
+ * @type {Function}
+ */
+exports.Client = Client;
+
+util.inherits(Client, events.EventEmitter);
+
+
+/**
+ * Establish a connection. Re-emits 'connect', 'error' and 'end' events from the underlaying connection
+ * @param callback - in format function(err) to receive error message
+ */
+Client.prototype.connect = function (callback) {
+    var self = this;
+    var connection = thrift.createConnection(self.options.host, self.options.port, {transport: thrift.TFramedTransport})
+        .on('error',function (e) {
+            self.emit('error', e);
+            callback(e);
+        }).on('connect',function () {
+            self.emit('connect');
+        }).on('end', function (e) {
+            self.emit('end');
+        });
+
+    var thriftClient = thrift.createClient(ImageService, connection);
+
+    self.connection = connection;
+    self.thriftClient = thriftClient;
+};
+
+/**
+ * Close the connection
+ * @param callback - in format function(err) to receive error message
+ */
+Client.prototype.end = function (callback) {
+    if (this.connection) {
+        this.connection.end(callback);
+    } else {
+        callback(new Error('Not connected yet.'));
+    }
+};
+
+/**
+ * ping function, to check server health status, 0 means server is okay.
+ * @param callback in format function(err, result), result is an integer
+ */
+Client.prototype.ping = function (callback) {
+    var self = this;
+    try {
+        this.thriftClient.ping(callback);
+    } catch (e) {
+        self.emit('error', e);
+        callback(e, null);
+    }
+};
+
+/**
+ * resize function, to resize
+ * @param callback in format function(err, result), result is an integer
+ */
+
+// main export function
+exports.createClient = function (options) {
+    return new Client(options);
+};
+
+
+// Helper functions
+
+// callback || noop borrowed from node/lib/fs.js
+function noop() {
+}
+
+
+function merge(a, b) {
+    if (a && b) {
+        for (var key in b) {
+            if (typeof a[key] == 'undefined') {
+                a[key] = b[key];
+            } else if (typeof a[key] == 'object' && typeof b[key] == 'object') {
+                a[key] = merge(a[key], b[key]);
+            }
+        }
+    }
+    return a;
+}
